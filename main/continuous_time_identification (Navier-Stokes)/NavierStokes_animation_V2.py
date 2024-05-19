bd9e1a469cd6077f80945d41837696b0e38b3131
SalisMaxima <112969827+SalisMaxima@users.noreply.github.com>
2024-05-19 16:45:59 +0200

    Animation

--- main/continuous_time_identification (Navier-Stokes)/NavierStokes_animation_V2.py
+++ main/continuous_time_identification (Navier-Stokes)/NavierStokes_animation_V2.py
@@ -41,6 +41,8 @@ fig, ax = plt.subplots(figsize=(10, 6), dpi=dpi)  # Increase figure size and DPI
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 
+#Starting with the exact pressure field
+print("Animating the exact pressure field...")
 # Function to initialize the plot
 def init():
     ax.clear()
@@ -75,6 +77,14 @@ ani = animation.FuncAnimation(fig, update, frames=range(T), init_func=init,inter
 
 # Save animation as GIF with higher DPI
 ani.save('./figures/pressure_exact.gif', writer='pillow', dpi=dpi)
+print("Animation saved successfully!")
+
+# Starting with the predicted pressure field
+print("Animating the predicted pressure field...")
+# Initialize the plot
+fig, ax = plt.subplots(figsize=(10, 6), dpi=dpi)  # Increase figure size and DPI
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
 
 # Then do the same for the predicted pressure field
 # The predicted pressure is p_pred
@@ -97,7 +107,7 @@ def init2():
 def update2(frame):
     ax.clear()
     # Interpolate the pressure data to the high-resolution grid for the current frame
-    P_frame = PP_start[:,:, frame]
+    P_frame = PP_stars[:,:, frame]
     
     h = ax.imshow(P_frame, interpolation='bicubic', cmap='rainbow', 
                   extent=[x_min, x_max, y_min, y_max], origin='lower', aspect='auto', vmin=p_min, vmax=p_max)
@@ -109,15 +119,17 @@ def update2(frame):
     return ax,
 
 # Create animation
-ani = animation.FuncAnimation(fig, update, frames=range(T), init_func=init,interval=80, blit=False)
+ani = animation.FuncAnimation(fig, update2, frames=range(T), init_func=init2,interval=80, blit=False)
 
 # Save animation as GIF with higher DPI
 ani.save('./figures/pressure_prediction.gif', writer='pillow', dpi=dpi)
-
+# Animation saved
+print("Animation saved successfully!")
 
 
 # Finally make a comparison between the exact and predicted pressure fields
 # Subtract the predicted pressure field from the exact pressure field
+print("Animating the pressure prediction error...")
 # Thus getting the error field
 PP_Error = P_exacts - PP_stars
 
@@ -155,5 +167,7 @@ ani3 = animation.FuncAnimation(fig, update3, frames=range(T), init_func=init3,in
 
 # Save animation as GIF with higher DPI
 ani3.save('./figures/pressure_prediction_error.gif', writer='pillow', dpi=dpi)
+
+print("Animation saved successfully!")
 
 
